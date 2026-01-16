25a2a97c8999098f2006d4ea8c2aac95e7c49280
CodeDccc <techietb7@gmail.com>
2026-01-16 17:01:04 -0500

    Refactor App and MenuBar components; remove index.css

--- src/MenuBar.tsx
+++ src/MenuBar.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
 const MenuBar: React.FC = () => {
-  return <div></div>;
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          My TV Shows
+        </Typography>
+        <Button color="inherit">Home</Button>
+        <Button color="inherit">Genre</Button>
+        <Button color="inherit">Favorites</Button>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default MenuBar;
