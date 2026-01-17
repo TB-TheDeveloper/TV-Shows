e36cd2aa43fd6f399b24e0ab2281bdaa41065866
CodeDccc <techietb7@gmail.com>
2026-01-16 23:03:55 -0500

    Add color to AppBar and include search icon in MenuBar

--- src/MenuBar.tsx
+++ src/MenuBar.tsx
@@ -3,10 +3,12 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import { IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 
 const MenuBar: React.FC = () => {
   return (
-    <AppBar position="static">
+    <AppBar position="static" color="secondary">
       <Toolbar>
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           My TV Shows
@@ -14,6 +16,9 @@ const MenuBar: React.FC = () => {
         <Button color="inherit">Home</Button>
         <Button color="inherit">Genre</Button>
         <Button color="inherit">Favorites</Button>
+        <IconButton color="inherit">
+          <SearchIcon />
+        </IconButton>
       </Toolbar>
     </AppBar>
   );
